71e60d7f186ee3f7ca7a41256f75fdc911e28e31
Cornelius Boateng <cob32@cornell.edu>
2022-04-04 02:46:13 -0400

    Create, get and drop routes created. Assignment and User Models added

---
+++ Projs/Abstraction with SQLAlchemy/src/db.py
@@ -0,0 +1,46 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# your classes here
+class Assignment(db.Model):
+    __tablename__ = "assignments"
+    id = db.Column(db.Integer, primary_key = True)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+
+class User(db.Model):
+    __tablename__ = "instructors"
+    id = db.Column(db.Integer, primary_key = True)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+
+class Course(db.Model):
+    __tablename__ = "courses"
+    id = db.Column(db.Integer, primary_key = True)
+    code = db.Column(db.String, nullable = False)
+    name = db.Column(db.String, nullable = False)
+    assignments = db.relationship("Assignment", cascade="delete")
+    instructors = db.relationship("Instructor", cascade="delete")
+    students = db.relationship("Student", cascade="delete")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "code": self.code,
+            "name": self.name,
+            "assignments": [assignment.serialize() for assignment in self.assignments],
+            "instructors": [instructor.serialize() for instructor in self.instructors],
+            "students": [student.serialize() for student in self.students]
+        }
